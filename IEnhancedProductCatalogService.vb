618eb2e362f2d2968d945883166b8200614179af
Todor Lilov <35934101+tlilov@users.noreply.github.com>
2018-01-31 18:24:59 -0800

    update
    update

--- IEnhancedProductCatalogService.vb
+++ IEnhancedProductCatalogService.vb
@@ -4,8 +4,11 @@ Public Interface IEnhancedProductCatalogService
 
     ReadOnly Property IsEnabled As Boolean
     Sub ExecuteCategoryProductListQuery(request As GetProductListRequest, response As GetProductListResponse)
-    Sub ExecuteNamedProductListQuery(request As GetNamedProductListRequest, response As GetNamedProductListResponse)
+        
+    Function ExecuteNamedProductListQuery(request As GetNamedProductListRequest) As GetNamedProductListResponse
+            
     Function GetCompleteRefinements() As List(Of RefinementGroup)
+                
     Sub ExecutePredictiveSearchQuery(request As GetPredictiveSearchRequest, response As GetPredictiveSearchResponse)
 
 End Interface
